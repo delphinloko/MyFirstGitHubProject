64d05eaa11d1422bfbb3749c5225b3c4e3d4729d
delphinloko <delphinloko@rambler.ru>
2019-09-24 09:43:54 +0300

    del

--- cs50/php/array.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $Arr = array ("a" => 1, "b" => "two", "c" => "three");
-    foreach ($Arr as $key => $value)
-    {
-        echo "Key: $key; Value: $value \n";
-    }
-
-?>
--- cs50/php/type-1.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $a = 1 + 1;
-    echo "1 + 1 is $a\n";
-    $typea = gettype($a);
-    echo "Type: $typea \n";
-?>
